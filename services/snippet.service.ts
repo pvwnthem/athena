84c4e90b9eb02aa1f0055168bd3c154722b998f6
pvwnthem <pvwn.them@gmail.com>
2023-08-06 21:22:02 -0400

    more helper funcs

--- services/snippet.service.ts
+++ services/snippet.service.ts
@@ -6,7 +6,28 @@ import { connect } from "@/lib/mongo";
 
 export async function createSnippet(snippet: Snippet) {
     await connect();
-    
+
     const result = await SnippetModel.create(snippet);
+    return result;
+}
+
+export async function getAllSnippets() {
+    await connect();
+
+    const result = await SnippetModel.find();
+    return result;
+}
+
+export async function getSnippetById(id: string) {
+    await connect();
+
+    const result = await SnippetModel.findById(id);
+    return result;
+}
+
+export async function updateSnippetById(id: string, snippet: Snippet) {
+    await connect();
+
+    const result = await SnippetModel.findByIdAndUpdate(id, snippet, { new: true });
     return result;
 }
