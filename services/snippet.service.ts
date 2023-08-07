444941dce808af75f3bbe95eefcb8becb48eba03
pvwnthem <pvwn.them@gmail.com>
2023-08-06 21:06:46 -0400

    createSnippet function

--- services/snippet.service.ts
+++ services/snippet.service.ts
@@ -1,7 +1,12 @@
 'use server'
 
 import { Snippet } from "@/types/snippet.type";
+import SnippetModel from "@/models/snippet";
+import { connect } from "@/lib/mongo";
 
 export async function createSnippet(snippet: Snippet) {
-
+    await connect();
+    
+    const result = await SnippetModel.create(snippet);
+    return result;
 }
