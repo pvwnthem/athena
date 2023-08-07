79c576bff21c4087cd61573209c6fca123dfd904
pvwnthem <pvwn.them@gmail.com>
2023-08-07 17:21:19 -0400

    shitty form

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+import React from 'react'
+
 export default function Page() {
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const code = formData.get("code");
+        const language = formData.get("language");
+        console.log(code, language);
+      }
     return (
         <>
-        
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="code" />
+                    <input type="text" name="language" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </>
     )
 }
