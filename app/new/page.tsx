3b8f375cbb20a5cf61a3725e42eb7003546e75c0
pvwnthem <pvwn.them@gmail.com>
2023-08-08 18:16:55 -0400

    terrible form

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -13,11 +13,19 @@ export default function Page() {
       }
     return (
         <>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="code" />
-                    <input type="text" name="language" />
-                    <button type="submit">Submit</button>
+            <div className="font-gfs-didot w-screen h-screen flex flex-col items-center justify-center">
+                <h1 className='font-semibold text-2xl'>Publish a Snippet</h1>
+                <div className='w-1/2 h-1 bg-red-800 rounded-full my-2'></div>
+                <form onSubmit={handleSubmit} className='md:w-1/2 flex flex-col rounded py-6 px-24 shadow-lg space-y-2 items-center justify-center'>
+                    <label htmlFor="code" className='text-xl self-start font-semibold'>Code</label>
+                    <textarea className="w-full border border-red-800 rounded" name="code" />
+                    <label htmlFor="code" className='text-xl self-start font-semibold'>Language</label>
+                    <select id="language" name="language" className="w-full self-start shadow-md rounded bg-white py-2 px-2 text-red-800">
+                        <option value="javascript">JavaScript</option>
+                        <option value="python">Python</option>
+                        <option value="ruby">Ruby</option>
+                    </select>
+                    <button type="submit" className='bg-red-800 py-2 px-8 text-white rounded-md'>Submit</button>
                 </form>
             </div>
         </>
